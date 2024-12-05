1cd3252130ada92d1ed5a7a69d6a8ab19b1c05c7
sergey-am-gm <sergey.am.gm@gmail.com>
2024-12-06 00:01:27 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,9 @@
+my_string = input('Введите текст: ')
+print('Введено количество символов',len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+my_string.replace(' ','')
+print(my_string[0])
+print(my_string[-1])
+
+
