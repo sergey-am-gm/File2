751329c5a434a2c1dd1b0d4939e3e87fe8e5d287
sergey-am-gm <sergey.am.gm@gmail.com>
2024-12-05 21:19:52 +0300

    Add files via upload

---
+++ Python file2.py
@@ -0,0 +1,5 @@
+Number_of_completed_homework = 12 #Количество выполненных ДЗ
+Number_of_hours_spent = 1.5 #Количество затраченных часов
+Course_name = 'Python' #Название курса
+Time_for_one_task = Number_of_hours_spent / Number_of_completed_homework #Время на одно задание
+print('Курс:',Course_name,', всего задач:',Number_of_completed_homework,', затрачено часов:',Number_of_hours_spent,', среднее время выполнения',Time_for_one_task,' часа.')
