99b1460f1274cafc3e735bf37c6421777e3654ac
sergey-am-gm <sergey.am.gm@gmail.com>
2024-12-05 20:26:22 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,12 @@
+from operator import truediv
+
+name = 'Sergei'
+print('Name: ',name)
+age = 58
+print('Age: ',age)
+age = age + 1
+print('New Age: ',age)
+is_student = True
+print('Is Student: ',is_student)
+
+
