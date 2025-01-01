3df8069b36bd097717313ac51ffbd2a6dc367172
sergey-am-gm <sergey.am.gm@gmail.com>
2025-01-02 00:54:26 +0300

    Add files via upload
    Домашняя работа по уроку "Функции в Python.Функция с параметром"

--- get_matrix.py
+++ get_matrix.py
@@ -7,6 +7,16 @@ def get_matrix(n, m, value):
         matrix.append(matrix_1)
     return matrix
 
+# или без вложенных циклов
+def get_matrix1(n, m, value):
+    matrix_1 = []
+    for j in range(m):
+        matrix_1.append(value)
+    matrix = []
+    for i in range(n):
+        matrix.append(matrix_1)
+    return matrix
+
 result1 = get_matrix(2, 2, 10)
 result2 = get_matrix(3, 5, 42)
 result3 = get_matrix(4, 2, 13)
